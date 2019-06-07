602cf49bbfc87a54e4665918af22203a630bd059
Xavier Carpentier <xcapetir@gmail.com>
2019-06-07 16:25:41 +0200

    fix activate job freely

--- both/lib/methods.js
+++ both/lib/methods.js
@@ -1,110 +1,116 @@
 Meteor.methods({
   deactivateJob: function(jobId, filled) {
-    check(jobId, String);
-    check(filled, Boolean);
+    check(jobId, String)
+    check(filled, Boolean)
 
     var job = Jobs.findOne({
-      _id: jobId
-    });
-    if (!job)
-      throw new Meteor.Error("Could not find job.");
+      _id: jobId,
+    })
+    if (!job) throw new Meteor.Error('Could not find job.')
 
     if (this.userId !== job.userId)
-      throw new Meteor.Error("You can only deactivate your own job.");
+      throw new Meteor.Error('You can only deactivate your own job.')
 
-    if (job.status !== "active")
-      throw new Meteor.Error("You can only deactivate an active job.");
+    if (job.status !== 'active')
+      throw new Meteor.Error('You can only deactivate an active job.')
 
-    Jobs.update({
-      _id: jobId
-    }, {
-      $set: {
-        status: (filled ? "filled" : "inactive")
-      }
-    });
+    Jobs.update(
+      {
+        _id: jobId,
+      },
+      {
+        $set: {
+          status: filled ? 'filled' : 'inactive',
+        },
+      },
+    )
   },
   adminSetJobStatus: function(jobId, status) {
-    check(jobId, String);
-    check(status, String);
+    check(jobId, String)
+    check(status, String)
 
     var job = Jobs.findOne({
-      _id: jobId
-    });
-    if (!job)
-      throw new Meteor.Error("Could not find job.");
+      _id: jobId,
+    })
+    if (!job) throw new Meteor.Error('Could not find job.')
 
     if (!Roles.userIsInRole(this.userId, ['admin']))
-      throw new Meteor.Error("Only admins can set job status");
+      throw new Meteor.Error('Only admins can set job status')
 
     var setObject = {
-      status: status
-    };
+      status: status,
+    }
 
-    if (Meteor.isServer && status === "active" && job.featured())
-      setObject.featuredThrough = moment().add(30, "days").toDate();
+    if (Meteor.isServer && status === 'active' && job.featured())
+      setObject.featuredThrough = moment()
+        .add(30, 'days')
+        .toDate()
 
-    Jobs.update({
-      _id: jobId
-    }, {
-      $set: setObject
-    });
-
+    Jobs.update(
+      {
+        _id: jobId,
+      },
+      {
+        $set: setObject,
+      },
+    )
   },
   adminSetProfileStatus: function(profileId, status) {
-    check(profileId, String);
-    check(status, String);
+    check(profileId, String)
+    check(status, String)
 
     var job = Profiles.findOne({
-      _id: profileId
-    });
-    if (!job)
-      throw new Meteor.Error("Could not find profile.");
+      _id: profileId,
+    })
+    if (!job) throw new Meteor.Error('Could not find profile.')
 
     if (!Roles.userIsInRole(this.userId, ['admin']))
-      throw new Meteor.Error("Only admins can set profile status");
+      throw new Meteor.Error('Only admins can set profile status')
 
     var setObject = {
-      status: status
-    };
+      status: status,
+    }
 
-
-    Profiles.update({
-      _id: profileId
-    }, {
-      $set: setObject
-    });
-
+    Profiles.update(
+      {
+        _id: profileId,
+      },
+      {
+        $set: setObject,
+      },
+    )
   },
-  jobActivate: function(tokenId, jobId) {
-    check(tokenId, String);
-    check(jobId, String);
+  jobActivate: function(_tokenId, jobId) {
+    // check(tokenId, String)
+    check(jobId, String)
 
-    var job = Jobs.findOne({ _id: jobId });
-    if (!job)
-      throw new Meteor.Error("Could not find job.");
+    var job = Jobs.findOne({ _id: jobId })
+    if (!job) throw new Meteor.Error('Could not find job.')
 
     if (job.userId !== this.userId)
-      throw new Meteor.Error("You can only pay for you own job post.");
-
+      throw new Meteor.Error('You can only pay for you own job post.')
 
     if (Meteor.isServer) {
-      const amount = job.codePromo === Meteor.settings.public.promo.early ? 5775 : 7700
-      var result = Stripe.charges.create({
-        source: tokenId,
-        amount,
-        currency: "eur",
-        description: "We hire React-Native - Job Post - 30 Days"
-      });
+      // const amount = job.codePromo === Meteor.settings.public.promo.early ? 5775 : 7700
+      // var result = Stripe.charges.create({
+      //   source: tokenId,
+      //   amount,
+      //   currency: "eur",
+      //   description: "We hire React-Native - Job Post - 30 Days"
+      // });
 
-      if (result && (result.status === "succeeded" || result.status === "paid")) { //'paid' status is not in stripe docs, but is occuring - see https://github.com/nate-strauser/wework/issues/108
-        Jobs.update({ _id: job._id }, {
+      //if (result && (result.status === "succeeded" || result.status === "paid")) { //'paid' status is not in stripe docs, but is occuring - see https://github.com/nate-strauser/wework/issues/108
+      Jobs.update(
+        { _id: job._id },
+        {
           $set: {
-            status: 'active'
-          }
-        });
-      } else {
-        throw new Meteor.Error("Payment Failed!", "Stripe result not as expected", JSON.stringify(result));
-      }
+            status: 'active',
+          },
+        },
+      )
+      // } else {
+      //   throw new Meteor.Error("Payment Failed!", "Stripe result not as expected", JSON.stringify(result));
+      // }
     }
-  }
-});
+  },
+})
--- client/views/jobs/jobIncludes.js
+++ client/views/jobs/jobIncludes.js
@@ -1,64 +1,77 @@
 Template.jobExpiredAlert.helpers({
   expired: function() {
     if (this.userId === Meteor.userId()) {
-      if ((this.createdAt < daysUntilExpiration()) && (this.updatedAt < daysUntilExpiration())) {
-        return true;
-      } else if ((this.createdAt < daysUntilExpiration()) && (this.updatedAt === undefined)) {
-        return true;
+      if (
+        this.createdAt < daysUntilExpiration() &&
+        this.updatedAt < daysUntilExpiration()
+      ) {
+        return true
+      } else if (
+        this.createdAt < daysUntilExpiration() &&
+        this.updatedAt === undefined
+      ) {
+        return true
       } else {
-        return false;
+        return false
       }
     } else {
-      return false;
+      return false
     }
-  }
-});
+  },
+})
 
 Template.jobStatusToggle.helpers({
-  "statuses": function() {
-    return STATUSES;
-  }
-});
+  statuses: function() {
+    return STATUSES
+  },
+})
 
 Template.jobStatusToggle.events({
-  "click .set-status": function(event, template) {
-    event.preventDefault();
-    Meteor.call("adminSetJobStatus", template.data._id, String(this));
-  }
-});
+  'click .set-status': function(event, template) {
+    event.preventDefault()
+    Meteor.call('adminSetJobStatus', template.data._id, String(this))
+  },
+})
 
 Template.jobActivate.events({
-  "click #job-activate": function(event, template) {
-    event.preventDefault();
-    var job = template.data;
-    var btn = $(event.currentTarget);
-    btn.button('loading');
-    const isPromo = Meteor.settings.public.promo.early === job.codePromo;
-    const amount = isPromo ? 5775 : 7700
-    const description = isPromo ? 'Job Post - 30 Days - 57.75€ (promo)' : 'Job Post - 30 Days - 77€'
-    StripeCheckout.open({
-      key: Meteor.settings.public.Stripe.pubKey,
-      name: 'We hire React-Native',
-      billingAddress: true,
-      allowRememberMe: true,
-      description,
-      currency: "eur",
-      amount,
-      email: getUserEmail(Meteor.user()),
-      closed: function() {
-        btn.button('reset');
-      },
-      token: function(token, args) {
-        Meteor.call("jobActivate", token.id, job._id, function(error, result) {
-          if (error) {
-            console.log("Insert Error:", error);
-          } else {
-            console.log(result);
-          }
-          btn.button('reset');
-        });
+  'click #job-activate': function(event, template) {
+    event.preventDefault()
+    var job = template.data
+    var btn = $(event.currentTarget)
+    btn.button('loading')
+    // const isPromo = Meteor.settings.public.promo.early === job.codePromo
+    // const amount = isPromo ? 5775 : 7700
+    // const description = isPromo
+    //   ? 'Job Post - 30 Days - 57.75€ (promo)'
+    //   : 'Job Post - 30 Days - 77€'
+    Meteor.call('jobActivate', token.id, job._id, function(error, result) {
+      if (error) {
+        console.log('Insert Error:', error)
       }
-    });
-  }
-});
-
+      btn.button('reset')
+    })
+    // StripeCheckout.open({
+    //   key: Meteor.settings.public.Stripe.pubKey,
+    //   name: 'We hire React-Native',
+    //   billingAddress: true,
+    //   allowRememberMe: true,
+    //   description,
+    //   currency: "eur",
+    //   amount,
+    //   email: getUserEmail(Meteor.user()),
+    //   closed: function() {
+    //     btn.button('reset');
+    //   },
+    //   token: function(token, args) {
+    //     Meteor.call("jobActivate", token.id, job._id, function(error, result) {
+    //       if (error) {
+    //         console.log("Insert Error:", error);
+    //       } else {
+    //         console.log(result);
+    //       }
+    //       btn.button('reset');
+    //     });
+    //   }
+    // });
+  },
+})
