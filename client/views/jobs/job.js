4d4c16c6fdef045cc4573670dcef2e8300191057
Xavier Carpentier <xcapetir@gmail.com>
2019-07-30 12:21:52 +0200

    fix canonical

--- client/views/jobs/job.js
+++ client/views/jobs/job.js
@@ -11,24 +11,7 @@ Template.job.events({
 
 Template.job.onRendered(function(_, __) {
   DocHead.setTitle(this.data.title)
-  DocHead.addMeta({
-    name: 'description',
-    content:
-      this.data.title + ' - ' + this.data.company + ' - ' + this.data.location,
-  })
-  DocHead.addMeta({
-    name: 'og:title',
-    content: this.data.title,
-  })
-  DocHead.addMeta({
-    name: 'og:description',
-    content:
-      this.data.title + ' - ' + this.data.company + ' - ' + this.data.location,
-  })
-  DocHead.addMeta({
-    name: 'og:site_name',
-    content: 'We hire React-Native',
-  })
+  DocHead.addLink({ rel: 'canonical', href: Router.current().originalUrl })
 })
 
 Template.job.helpers({
